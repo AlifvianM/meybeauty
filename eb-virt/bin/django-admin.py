70c29596c8c0f0bbee9443b2ab0cb97e7a6efa41
AlifvianM <alifvianm@gmail.com>
2020-08-23 21:26:36 +0700

    alpha 2

--- eb-virt/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alifvianmarco/Documents/project/drwecommerce/meybeauty/eb-virt/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
